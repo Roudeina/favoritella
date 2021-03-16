b6632175fa7caa939eb3d2f045292544ce2cccaf
Roudeina <roudeina.bentrad@gmail.com>
2021-03-16 16:00:29 +0100

    favorites model

---
+++ server/models/favorites.model.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, Sequelize) => {
+  const Farovites = sequelize.define("farovites", {
+    title: {
+      type: Sequelize.STRING
+    },
+    actors: {
+      type: Sequelize.STRING
+    },
+    review: {
+      type: Sequelize.NUMBER
+    },
+    date: {
+      type: Sequelize.STRING
+    },
+    image: {
+      type: Sequelize.STRING
+    },
+  });
+
+  return Farovites;
+};
